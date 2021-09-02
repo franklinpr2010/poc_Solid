b62e4d0e9dfda96a5b334b600eebad80858cf17a
GFT\fnpa <franklinpr2010@gmail.com>
2021-09-02 14:42:42 -0300

    adicionando projeto solid

---
+++ solid-java-projeto_inicial/src/br/com/alura/rh/service/ValidacaoPeriodicidadeEntreReajuste.java
@@ -0,0 +1,20 @@
+package br.com.alura.rh.service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+
+public class ValidacaoPeriodicidadeEntreReajuste {
+	public void validar(Funcionario funcionario, BigDecimal aumento) {
+		LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+		LocalDate dataAtual = LocalDate.now();
+		long mesesDeseUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+		if(mesesDeseUltimoReajuste < 6) {
+			throw new ValidacaoException("Intervalo entre reajustes deve ser maior que 6 meses!");
+		}
+	}
+}
